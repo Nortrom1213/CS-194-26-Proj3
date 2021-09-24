b44ce39fca0e63816194970203766fd5bce3016e
Nortrom1213 <nortrom@berkeley.edu>
2021-09-23 21:35:10 -0700

    Update

--- main.py
+++ main.py
@@ -30,36 +30,38 @@ def define_points(num_points, image):
     return points
 
 # read img
-img_target = plt.imread('depp.jpg')
-img_source = plt.imread('me.jpg')
-img_target = sk.transform.resize(img_target, IMG_SHAPE)
-img_source = sk.transform.resize(img_source, IMG_SHAPE)
+target = plt.imread('depp.jpg')
+source = plt.imread('me.jpg')
+target = sk.transform.resize(target, IMG_SHAPE)
+source = sk.transform.resize(source, IMG_SHAPE)
 # select points
-if os.path.exists('points_target.npy'):
-    points_target = np.load('points_target.npy')
-    points_source = np.load('points_source.npy')
+if os.path.exists('target_points.npy'):
+    target_points = np.load('target_points.npy')
+    source_points = np.load('source_points.npy')
 else:
-    points_target = define_points(NUM_POINTS, img_target)
-    points_source = define_points(NUM_POINTS, img_source)
+    target_points = define_points(NUM_POINTS, target)
+    source_points = define_points(NUM_POINTS, source)
     # save points
-    np.save("points_target.npy", points_target)
-    np.save("points_source.npy", points_source)
+    np.save("target_points.npy", target_points)
+    np.save("source_points.npy", source_points)
 
 
 corners = np.array([
     [0, 0],
-    [IMG_SHAPE[1]-1, 0], [0, IMG_SHAPE[0]-1],
+    [IMG_SHAPE[1]-1, 0],
+    [0, IMG_SHAPE[0]-1],
     [IMG_SHAPE[1]-1, IMG_SHAPE[0]-1]
 ])
-points_target = np.vstack([points_target, corners])
-points_source = np.vstack([points_source, corners])
+target_points = np.vstack([target_points, corners])
+source_points = np.vstack([source_points, corners])
+
 
 # triangulation
-mid_points = (points_target + points_source)/2
+mid_points = (target_points + source_points)/2
 triangulation = Delaunay(mid_points)
 
 plt.figure(figsize=(20, 10))
-for i, (img, points) in enumerate(zip((img_target, img_source), (points_target, points_source))):
+for i, (img, points) in enumerate(zip((target, source), (target_points, source_points))):
     plt.subplot(1, 2, i+1)
     plt.imshow(img)
     plt.triplot(points[:,0], points[:,1], triangulation.simplices)
@@ -79,12 +81,12 @@ def computeAffine(tri1_pts, tri2_pts):
     return np.dot(trans(tri2_pts), np.linalg.inv(trans(tri1_pts)))
 
 
-def compute_masks(img_target, img_source, points_target, points_source, triangulation, warp_frac, dissolve_frac):
+def compute_masks(target, source, target_points, source_points, triangulation, warp_frac, dissolve_frac):
     if warp_frac < 0 or warp_frac > 1 or dissolve_frac < 0 or dissolve_frac > 1:
         return None
 
     masks = [np.zeros((IMG_SHAPE[0], IMG_SHAPE[1], 3)) for _ in range(3)]
-    mid_pts = points_source + warp_frac * (points_target - points_source)
+    mid_pts = source_points + warp_frac * (target_points - source_points)
 
     for tri_index in triangulation.simplices:
         mid_tri = mid_pts[tri_index]
@@ -92,22 +94,22 @@ def compute_masks(img_target, img_source, points_target, points_source, triangul
         mid_xys = np.array([rr, cc])
         tmp = np.vstack([mid_xys, np.ones((1, mid_xys.shape[1]))])
 
-        src1_tri = points_target[tri_index]
+        src1_tri = target_points[tri_index]
         mat_T1 = np.linalg.inv(computeAffine(src1_tri, mid_tri))
         src1_xys = np.dot(mat_T1, tmp).astype(int)
-        masks[0][mid_xys[1], mid_xys[0]] = img_target[src1_xys[1], src1_xys[0]]
+        masks[0][mid_xys[1], mid_xys[0]] = target[src1_xys[1], src1_xys[0]]
 
-        src2_tri = points_source[tri_index]
+        src2_tri = source_points[tri_index]
         mat_T2 = np.linalg.inv(computeAffine(src2_tri, mid_tri))
         src2_xys = np.dot(mat_T2, tmp).astype(int)
-        masks[2][mid_xys[1], mid_xys[0]] = img_source[src2_xys[1], src2_xys[0]]
+        masks[2][mid_xys[1], mid_xys[0]] = source[src2_xys[1], src2_xys[0]]
 
     masks[1] = masks[2] + dissolve_frac * (masks[0] - masks[2])
     return masks
 
 
 plt.figure(figsize=(15, 5))
-masks = compute_masks(img_target, img_source, points_target, points_source, triangulation, 0.5, 0.5)
+masks = compute_masks(target, source, target_points, source_points, triangulation, 0.5, 0.5)
 for i in range(3):
     plt.subplot(1, 3, i + 1)
     plt.imshow(masks[i])
@@ -116,13 +118,13 @@ plt.savefig('midway_face.jpg')
 NUM_FRAMES = 45
 
 def morph(im1, im2, pts1, pts2, tri, warp_frac, dissolve_frac):
-    return compute_masks(img_target, img_source, points_target, points_source, triangulation, x, x)[1]
+    return compute_masks(target, source, target_points, source_points, triangulation, x, x)[1]
 
 # generate morphing
 results = []
 fig = plt.figure(figsize=(8, 10))
 for x in np.linspace(0, 1, NUM_FRAMES):
-    img = morph(img_target, img_source, points_target, points_source, triangulation, x, x)
+    img = morph(target, source, target_points, source_points, triangulation, x, x)
     img = plt.imshow(img, animated=True)
     results.append([img])
 
